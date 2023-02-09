d2dd90f3b8f328c754a39f9f73e02d220a2820d2
Mayank Soni <mayank.soni@mayanksoni.tech>
2023-02-08 17:19:59 -0700

    HSD-1 Initial Commit

---
+++ src/main/java/com/heimdallauth/utils/ServiceDeployerApplication.java
@@ -0,0 +1,13 @@
+package com.heimdallauth.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDeployerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDeployerApplication.class, args);
+    }
+
+}
