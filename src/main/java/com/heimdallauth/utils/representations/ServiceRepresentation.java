c64973c91b98a7a3ad96ab445f7fb2bb2303515f
Mayank Soni <mayank.soni@mayanksoni.tech>
2023-02-08 20:46:22 -0700

    HSD-9 Organize Imports

--- src/main/java/com/heimdallauth/utils/representations/ServiceRepresentation.java
+++ src/main/java/com/heimdallauth/utils/representations/ServiceRepresentation.java
@@ -1,15 +1,10 @@
 package com.heimdallauth.utils.representations;
 
 import com.heimdallauth.utils.constants.ContainerRestartPolicy;
-import com.heimdallauth.utils.constants.ServiceConstants;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-
-import java.time.Instant;
-import java.util.Map;
 
 @SuperBuilder
 @NoArgsConstructor
@@ -18,6 +13,4 @@ import java.util.Map;
 public class ServiceRepresentation {
     private ImageRepresentation image;
     private ContainerRestartPolicy restartPolicy;
-
-
 }
